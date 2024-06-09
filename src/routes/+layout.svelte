b021843235ce4f87b9fc62b8bce0969ffbf829fe
vangona <rlarhksrud12@naver.com>
2024-06-10 00:57:25 +0900

    feat: LanguageSelect Transition 구현

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
-<script>
+<script lang="ts">
 	import * as Select from '$lib/components/ui/select/index';
 	import '../app.css';
 	import { getLocaleFromNavigator, init, register, waitLocale } from 'svelte-i18n';
+	import { slide } from 'svelte/transition';
+	import { onMount, onDestroy } from 'svelte';
 
 	register('ko', () => import('../locale/ko.json'));
 	register('en', () => import('../locale/en.json'));
@@ -12,12 +14,33 @@
 	});
 
 	const fruits = [
-		{ value: 'apple', label: 'Apple' },
-		{ value: 'banana', label: 'Banana' },
-		{ value: 'blueberry', label: 'Blueberry' },
-		{ value: 'grapes', label: 'Grapes' },
-		{ value: 'pineapple', label: 'Pineapple' }
+		{ value: 'ko', label: '한국어' },
+		{ value: 'en', label: 'English' },
+		{ value: 'zh-Hans', label: '简体中文' },
+		{ value: 'ja', label: '日本語' },
+		{ value: 'vi', label: 'Tiếng Việt' },
+		{ value: 'th', label: 'แบบไทย' }
 	];
+
+	let selectedOption: { label?: string; value: string } | undefined;
+	let languageIndex = 0;
+	let languageStr = ['Language', '언어', '语言', '言語', 'ngôn ngữ', 'ภาษา'];
+	let roller: number;
+
+	onMount(() => {
+		roller = setInterval(() => {
+			if (languageIndex < languageStr.length - 1) {
+				languageIndex++;
+				return;
+			}
+
+			if (languageIndex === languageStr.length - 1) languageIndex = 0;
+		}, 3000);
+	});
+
+	onDestroy(() => {
+		clearInterval(roller);
+	});
 
 	export async function preload() {
 		return waitLocale();
@@ -26,13 +49,31 @@
 
 <main class="h-full">
 	<nav class="fixed right-5 top-5">
-		<Select.Root>
+		<Select.Root
+			selected={selectedOption}
+			onSelectedChange={(value) => {
+				value && (selectedOption = value);
+			}}
+		>
 			<Select.Trigger class="w-[180px]">
-				<Select.Value placeholder="Select a fruit" />
+				<Select.Value asChild>
+					<div class="min-w-[100px] text-body2">
+						{#if selectedOption}
+							<div>
+								{selectedOption.label}
+							</div>
+						{:else}
+							{#key languageIndex}
+								<div transition:slide>
+									{languageStr[languageIndex]}
+								</div>
+							{/key}
+						{/if}
+					</div>
+				</Select.Value>
 			</Select.Trigger>
 			<Select.Content class="z-50">
 				<Select.Group>
-					<Select.Label>Fruits</Select.Label>
 					{#each fruits as fruit}
 						<Select.Item value={fruit.value} label={fruit.label}>{fruit.label}</Select.Item>
 					{/each}
